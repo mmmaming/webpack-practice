715bc01b31382b88c77abcfbfb83dbbceaf5917c
mmmaming <864845434@qq.com>
2018-03-05 14:35:35 +0800

    webpack

---
+++ src/index2.js
@@ -0,0 +1,4 @@
+/**
+ * Created on 2018/3/1.
+ */
+console.log('hello index1111111');
---
+++ src/print.js
@@ -0,0 +1,6 @@
+/**
+ * Created on 2018/3/5.
+ */
+export default function printMe() {
+  console.log('I get called from print.js!');
+}
