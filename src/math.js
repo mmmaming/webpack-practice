33e14244f5631e4718b8cd40db129277f796c841
mmmaming <864845434@qq.com>
2018-03-06 10:45:17 +0800

    webpack

---
+++ src/math.js
@@ -0,0 +1,25 @@
+/**
+ * Created on 2018/3/6.
+ */
+export function square(x) {
+  return x * x;
+}
+
+export function cube(x) {
+  return x * x * x;
+}
+export function cube1(x) {
+  return x * x * x;
+}
+export function cube2(x) {
+  return x * x * x;
+}
+export function cube3(x) {
+  return x * x * x;
+}
+export function cube4(x) {
+  return x * x * x;
+}
+export function cube5(x) {
+  return x * x * x;
+}
--- src/print.js
+++ src/print.js
@@ -2,5 +2,5 @@
  * Created on 2018/3/5.
  */
 export default function printMe() {
-  console.log('I get called from print.js!');
+  console.log('BUpdating print.js...')
 }
