2c13e57a31d098086537a070d1c9041b04087561
mmmaming <maming@mamingdeMacBook-Pro.local>
2019-01-23 17:24:42 +0800

    支持react

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-// main.ts
-// 通过 ES6 模块规范导入 show 函数
-// require('./main.css');
-import {show} from './show';
-import './main.css';
-// 执行 show 函数
-show('Webpack');
+import * as React from 'react';
+import { Component } from 'react';
+import { render } from 'react-dom';
+
+class Button extends Component {
+  render() {
+    return <React.Fragment>Hello,Webpack！！</React.Fragment>
+  }
+}
+
+render(<Button/>, window.document.getElementById('app'));
