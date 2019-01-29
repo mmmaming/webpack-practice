885cd0762ededd484120b29e79a12379d738dd60
mmmaming <maming@mamingdeMacBook-Pro.local>
2019-01-29 16:57:03 +0800

    单页应用生成HTML

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
-
+import './main.css';
 class Button extends Component {
   render() {
     return <React.Fragment>Hello,Webpack！！</React.Fragment>
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 console.log(__dirname);
 console.log(path.resolve(__dirname, './dist'));
 module.exports = {
   // JavaScript 执行入口文件
-  entry: './main.js',
+  entry: {
+    app: './main.js'
+  },
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'bundle.js',
+    filename: '[name]_[chunkhash:8].js',
     // 把输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
   },
@@ -33,9 +38,35 @@ module.exports = {
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.html',
+      filename: 'index.html'
+    }),
     new ExtractTextPlugin({
       // 从 .js 文件中提取出来的 .css 文件的名称
       filename: `[name]_[contenthash:8].css`,
     }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      // 最紧凑的输出
+      beautify: false,
+      // 删除所有的注释
+      comments: false,
+      compress: {
+        // 在UglifyJs删除没有用到的代码时不输出警告
+        warnings: false,
+        // 删除所有的 `console` 语句，可以兼容ie浏览器
+        drop_console: true,
+        // 内嵌定义了但是只用到一次的变量
+        collapse_vars: true,
+        // 提取出出现多次但是没有定义成变量去引用的静态值
+        reduce_vars: true,
+      }
+    })
+
   ]
 };
