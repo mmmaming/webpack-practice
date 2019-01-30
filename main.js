bd4d0cff81a5cf081b0a6a837b996012b4cbf978
mmmaming <maming@mamingdeMacBook-Pro.local>
2019-01-30 10:24:32 +0800

    add npmpackage

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ import * as React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
 import './main.css';
+import webpackPractice from 'webpack-practice-mm';
 class Button extends Component {
   render() {
-    return <React.Fragment>Hello,Webpack！！</React.Fragment>
+    return <React.Fragment>Hello,Webpack！！
+
+<webpackPractice />
+
+    </React.Fragment>
   }
 }
 
