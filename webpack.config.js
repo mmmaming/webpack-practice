da26f667d6f370bfdc724ff3014a84e40a0a88b7
mmmaming <maming@mamingdeMacBook-Pro.local>
2019-01-30 10:22:10 +0800

    npm 发布 一个包

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,9 @@ module.exports = {
     app: './main.js'
   },
   output: {
-    // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: '[name]_[chunkhash:8].js',
-    // 把输出文件都放到 dist 目录下
-    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: 'commonjs2'
   },
   devtool: 'source-map',
   module: {
@@ -20,13 +19,9 @@ module.exports = {
       {
         // 用正则去匹配要用该 loader 转换的 CSS 文件
         test: /\.css$/,
-        use: ['style-loader', {
-          loader: 'css-loader',
-          options: {
-            minimize: true
-          }
-        },
-          'postcss-loader'],
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader']
+        }),
       },
       {
         test: /\.jsx?$/,
@@ -36,35 +31,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './template.html',
-      filename: 'index.html'
-    }),
+    
     new ExtractTextPlugin({
       // 从 .js 文件中提取出来的 .css 文件的名称
-      filename: `[name]_[contenthash:8].css`,
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      // 最紧凑的输出
-      beautify: false,
-      // 删除所有的注释
-      comments: false,
-      compress: {
-        // 在UglifyJs删除没有用到的代码时不输出警告
-        warnings: false,
-        // 删除所有的 `console` 语句，可以兼容ie浏览器
-        drop_console: true,
-        // 内嵌定义了但是只用到一次的变量
-        collapse_vars: true,
-        // 提取出出现多次但是没有定义成变量去引用的静态值
-        reduce_vars: true,
-      }
+      filename: `[name]-index.css`,
     })
-
-  ]
+  ],
+  externals: /^(react|babel-runtime)/
 };
